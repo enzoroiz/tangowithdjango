3ad027b79a13d6c4d327c683924ed82c43e481c2
Enzo Roiz <enzoroiz@gmail.com>
2015-01-26 11:17:46 +0000

    Chapter 6 - completed

--- tangowithdjango/settings.py
+++ tangowithdjango/settings.py
@@ -58,10 +58,12 @@ WSGI_APPLICATION = 'tangowithdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+DATABASE_PATH = os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': DATABASE_PATH,
     }
 }
 

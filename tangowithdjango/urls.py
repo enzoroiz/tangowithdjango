84dad7c2d782d696e79fd4174cbaaca2fdc1a72f
Enzo Roiz <enzoroiz@gmail.com>
2015-02-02 21:53:16 +0000

    Chapter 12 - completed

--- tangowithdjango/urls.py
+++ tangowithdjango/urls.py
@@ -4,14 +4,19 @@ from django.conf.urls import patterns, url
 from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/rango/'
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tangowithdjango.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+        #Add in this url pattern to override the default pattern in accounts.
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    (r'^accounts/', include('registration.backends.simple.urls')),
 )
 
 if settings.DEBUG:
